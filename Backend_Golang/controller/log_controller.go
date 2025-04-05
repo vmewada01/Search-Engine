17b7ec19a47726ed73852375f81cc134f5309d83
vmewada01 <vishalmewada9826@gmail.com>
2025-04-05 19:10:32 +0530

    remove the move function from the code

--- Backend_Golang/controller/log_controller.go
+++ Backend_Golang/controller/log_controller.go
@@ -195,29 +195,3 @@ func extractColumnTypes(path string) (map[string]string, error) {
 
 	return colTypes, nil
 }
-
-func moveFile(src, dst string) error {
-	sourceFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
-
-	destFile, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer destFile.Close()
-
-	_, err = io.Copy(destFile, sourceFile)
-	if err != nil {
-		return err
-	}
-
-	err = sourceFile.Close() // explicitly close before delete
-	if err != nil {
-		return err
-	}
-
-	return os.Remove(src)
-}
